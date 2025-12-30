1ae6be68d9aa399721dbfdf7a6ad312cfe800226
Nathalie <114744839+cNathalie@users.noreply.github.com>
2025-12-30 15:32:57 +0100

    Updates

---
+++ assets/Basisprincipes-BJ0hA2hZ.js
@@ -0,0 +1,28 @@
+import{j as e}from"./index-u4Ysj0KW.js";const i="/web2-cursus-2026/images/browsers.png",a="/web2-cursus-2026/images/nodejs.jpg",t="/web2-cursus-2026/images/script-tag.png";function s(r){const n={code:"code",h2:"h2",hr:"hr",li:"li",p:"p",pre:"pre",ul:"ul",...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"JavaScript run-time omgeving"}),`
+`,e.jsx(n.p,{children:"JavaScript is een programmeertaal die gebruikt wordt om webpagina's interactief te maken. JavaScript wordt uitgevoerd in de browser van de gebruiker. De browser is de run-time omgeving van JavaScript."}),`
+`,e.jsx("img",{src:i,alt:"Browsers",className:"img-fluid my-3"}),`
+`,e.jsx(n.p,{children:"JavaScript kan ook gebruikt worden om servers te programmeren. In dat geval wordt JavaScript uitgevoerd in een server-side run-time omgeving zoals Node.js."}),`
+`,e.jsx("img",{src:a,alt:"NodeJs",className:"img-fluid my-3"}),`
+`,e.jsx(n.hr,{}),`
+`,e.jsx(n.h2,{children:"Wat doet JavaScript"}),`
+`,e.jsx(n.p,{children:"JavaScript kan gebruikt worden om de inhoud van een webpagina te veranderen."}),`
+`,e.jsx(n.p,{children:"Je brengt de statische html-pagina, door middel van DOM-manipulatie, tot leven door het stylen van elementen, het toevoegen van interactie en het manipuleren van de inhoud. Het kan ook gebruikt worden voor het versturen van HTTP-requests en het verwerken van de response."}),`
+`,e.jsx(n.hr,{}),`
+`,e.jsxs(n.h2,{children:["JavaScript en HTML: het ",e.jsx(n.code,{children:"<script>"})," element"]}),`
+`,e.jsxs(n.p,{children:["JavaScript kan in een HTML-bestand geplaatst worden. Je kan JavaScript code in een HTML-bestand schrijven tussen de ",e.jsx(n.code,{children:"<script>"})," tags."]}),`
+`,e.jsxs(n.p,{children:["Je kan ook een extern JavaScript-bestand linken aan een HTML-bestand. Dit doe je door een ",e.jsx(n.code,{children:"<script>"})," tag te gebruiken met een ",e.jsx(n.code,{children:"src"})," attribuut."]}),`
+`,e.jsx(n.p,{children:"Bijvoorbeeld:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<script src="script.js"><\/script>
+`})}),`
+`,e.jsxs(n.p,{children:["Het is belangrijk om de uitvoering van de JavaScript code te laten wachten tot de HTML pagina volledig geladen is. Daarom plaats je de ",e.jsx(n.code,{children:"<script>"})," tag volledig onderaan, vlak voor de ",e.jsx(n.code,{children:"</body> "}),"tag."]}),`
+`,e.jsx("img",{src:t,alt:"Script",className:"img-fluid my-3"}),`
+`,e.jsx(n.h2,{children:e.jsx(n.code,{children:"console.log()"})}),`
+`,e.jsxs(n.p,{children:["De ",e.jsx(n.code,{children:"console.log()"})," methode wordt gebruikt om berichten te loggen naar de console van de browser. Dit is handig om te controleren of je code correct werkt en om waarden van variabelen te bekijken. Tussen de haakjes van ",e.jsx(n.code,{children:"console.log()"}),"plaats je de waarde of de naam van de variabele die je wil loggen."]}),`
+`,e.jsx(n.p,{children:"Probeer het eens uit de in de Console van je Browser Developer Tools:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"console.log(new Date().toString())"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"console.log('Dit is de cursus Web 2')"})}),`
+`]}),`
+`,e.jsx(n.p,{children:"Vergelijk met C#:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-C#",children:`Console.WriteLine()
+`})})]})}function d(r={}){const{wrapper:n}=r.components||{};return n?e.jsx(n,{...r,children:e.jsx(s,{...r})}):s(r)}export{d as default};
